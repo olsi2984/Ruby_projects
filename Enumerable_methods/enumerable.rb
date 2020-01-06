9aebe608ac5cbac92f0b39e692c53bc1e4e08d88
Olsi Qatipi <olsi2984@yahoo.com>
2020-01-06 22:48:11 +0100

    Completed some enumerables

--- Enumerable_methods/enumerable.rb
+++ Enumerable_methods/enumerable.rb
@@ -14,10 +14,80 @@ module Enumerable
             i +=1
         end
     end
+    def my_select
+        new_arr = []
+        self.my_each do |n|
+            if yield(n)
+                new_arr.push(n)
+            end
+        end
+        print new_arr
+    end
+    def my_all?
+        self.my_each do |n|
+            if !yield(n)
+                return false
+            end
+        end
+        return true
+    end
+    def my_any?
+        self.my_each do |n|
+            if yield(n)
+                return true
+            end
+        end
+        return false
+    end
+    def my_none?
+        self.my_each do |n|
+            if yield(n)
+                return false
+            end
+        end
+        return true
+    end
+    def my_count item = nil
+        counter = 0
+        if block_given?
+            self.my_each do |n|
+                if yield(n)
+                    counter +=1
+                end
+            end
+            return counter
+        end
+        if item
+            self.my_each do |n|
+                if n === item
+                    counter +=1
+                end
+            end
+            return counter
+        else
+            return self.length
+        end
+    end
+    def my_map
+        map_arr = []
+        self.my_each do |n|
+            map_arr.push yield(n)
+        end
+        return map_arr
+    end
+    def my_inject
+        
+    end
     
 end
 
-([1,2,3,7]).each_with_index do |n, index|
-    stringer = index.to_s + "." + n.to_s
-    puts stringer
-end
+res1 = [1,2,3,4,7].my_map{ |x| x%2==0 }
+res2 = [1,2,5,6,2].my_map{|k| k*2}
+res3 = ["ant", "bear", "cat"].my_map{|l| l = l + "y"}
+puts ""
+print res1
+puts ""
+print res2
+puts ""
+print res3
+puts ""
