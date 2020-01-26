ed94943b9e392a11cfb5aea117c361c981283535
Olsi Qatipi <olsi2984@yahoo.com>
2020-01-26 23:34:39 +0100

    Added game validations and made some refactoring

--- Tic_Tac_Toe/tic_tac_toe.rb
+++ Tic_Tac_Toe/tic_tac_toe.rb
@@ -1,15 +1,10 @@
-module GameValidations
-  #Allow only 1-9 numbers
-  #Doesn't allow overwriting cell value"
-end
-
 class Game
   
-  #include GameValidations
-
   @@message = "It's a tie"
-
-  def initalize(board, tot_counter, message)
+  @@p1_played = "false"
+  @@p2_played = "false"
+  
+  def initalize(board, tot_counter)
     @board = board
     @tot_counter = tot_counter
   end
@@ -23,56 +18,25 @@ class Game
   end
 
   def p1_turn(board)
+    @@p2_played = "false"
     puts "Player 1 please type a number from 1 to 9 where do you want to put your x"
-    p1 = gets.chomp.to_i
-    board[p1-1] = "x"
-    show_board(board)
-  end
-  
-  def p2_turn(board)
-    puts "Player 2 please type a number from 1 to 9 where do you want to put your o"
-    p2 = gets.chomp.to_i
-    board[p2-1] = "o"
+    p = gets.chomp.to_i
+    puts "Please type a valid input" if p.between?(1, 9) == false
+    puts "Sorry that cell is taken" if board[p-1] == "x" || board[p-1] == "o"
+    board[p-1] = "x" if p.between?(1, 9) && board[p-1] == " "
+    @@p1_played = "true" if board[p-1] == "x"
     show_board(board)
   end
 
-  def check_winner(board)
-    
-    if board[0] == "x" && board[1] == "x" && board[2] == "x"
-        puts @@message = "Player 1 wins"
-      elsif board[0] == "o" && board[1] == "o" && board[2] == "o"
-        puts @@message = "Player 2 wins"
-      elsif board[3] == "x" && board[4] == "x" && board[5] == "x"
-        puts @@message = "Player 1 wins"
-      elsif board[3] == "o" && board[4] == "o" && board[5] == "o"
-        puts @@message = "Player 2 wins"
-      elsif board[6] == "x" && board[7] == "x" && board[8] == "x"
-        puts @@message = "Player 1 wins"
-      elsif board[6] == "o" && board[7] == "o" && board[8] == "o"
-        puts @@message = "Player 2 wins"
-      elsif board[0] == "x" && board[3] == "x" && board[6] == "x"
-        puts @@message = "Player 1 wins"
-      elsif board[0] == "o" && board[3] == "o" && board[6] == "o"
-        puts @@message = "Player 2 wins"
-      elsif board[1] == "x" && board[4] == "x" && board[7] == "x"
-        puts @@message = "Player 1 wins"
-      elsif board[1] == "o" && board[4] == "o" && board[7] == "o"
-        puts @@message = "Player 2 wins"
-      elsif board[2] == "x" && board[5] == "x" && board[8] == "x"
-        puts @@message = "Player 1 wins"
-      elsif board[2] == "o" && board[5] == "o" && board[8] == "o"
-        puts @@message = "Player 2 wins"
-      elsif board[0] == "x" && board[4] == "x" && board[8] == "x"
-        puts @@message = "Player 1 wins"
-      elsif board[0] == "o" && board[4] == "o" && board[8] == "o"
-        puts @@message = "Player 2 wins"
-      elsif board[2] == "x" && board[4] == "x" && board[6] == "x"
-        puts @@message = "Player 1 wins"
-      elsif board[2] == "o" && board[4] == "o" && board[6] == "o"
-        puts @@message = "Player 2 wins"
-      else
-        @@message = "It's a tie"
-    end
+  def p2_turn(board)
+    @@p1_played = "false"
+    puts "Player 2 please type a number from 1 to 9 where do you want to put your o"
+    p = gets.chomp.to_i
+    puts "Please type a valid input" if p.between?(1, 9) == false
+    puts "Sorry that cell is taken" if board[p-1] == "x" || board[p-1] == "o"
+    board[p-1] = "o" if p.between?(1, 9) && board[p-1] == " "
+    @@p2_played = "true" if board[p-1] == "o"
+    show_board(board)
   end
 
   def play_game(board) 
@@ -82,7 +46,7 @@ class Game
       p1_turn(board)
       check_winner(board)
       return if @@message == "Player 1 wins" || @@message == "Player 2 wins"
-      p2_turn(board) 
+      p2_turn(board)
       check_winner(board) 
       return if @@message == "Player 1 wins" || @@message == "Player 2 wins"
       tot_counter += 2  
@@ -94,8 +58,26 @@ class Game
     end
   end
 
-end
+  def check_winner(board)   
+    puts @@message = "Player 1 wins" if board[0] == "x" && board[1] == "x" && board[2] == "x"    
+    puts @@message = "Player 2 wins" if board[0] == "o" && board[1] == "o" && board[2] == "o"
+    puts @@message = "Player 1 wins" if board[3] == "x" && board[4] == "x" && board[5] == "x"
+    puts @@message = "Player 2 wins" if board[3] == "o" && board[4] == "o" && board[5] == "o"
+    puts @@message = "Player 1 wins" if board[6] == "x" && board[7] == "x" && board[8] == "x"
+    puts @@message = "Player 2 wins" if board[6] == "o" && board[7] == "o" && board[8] == "o"
+    puts @@message = "Player 1 wins" if board[0] == "x" && board[3] == "x" && board[6] == "x"
+    puts @@message = "Player 2 wins" if board[0] == "o" && board[3] == "o" && board[6] == "o"
+    puts @@message = "Player 1 wins" if board[1] == "x" && board[4] == "x" && board[7] == "x"
+    puts @@message = "Player 2 wins" if board[1] == "o" && board[4] == "o" && board[7] == "o"
+    puts @@message = "Player 1 wins" if board[2] == "x" && board[5] == "x" && board[8] == "x"
+    puts @@message = "Player 2 wins" if board[2] == "o" && board[5] == "o" && board[8] == "o"
+    puts @@message = "Player 1 wins" if board[0] == "x" && board[4] == "x" && board[8] == "x"
+    puts @@message = "Player 2 wins" if board[0] == "o" && board[4] == "o" && board[8] == "o"
+    puts @@message = "Player 1 wins" if board[2] == "x" && board[4] == "x" && board[6] == "x"
+    puts @@message = "Player 2 wins" if board[2] == "o" && board[4] == "o" && board[6] == "o"
+  end
 
+end
 
 ttt = Game.new
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
