348a893a0adc56e105e1228b79d81612d1e6f87c
Olsi Qatipi <olsi2984@yahoo.com>
2020-03-29 22:03:11 +0200

    event manager complete

--- event_manager/event_manager_weekday_targeting.rb
+++ event_manager/event_manager_weekday_targeting.rb
@@ -3,7 +3,7 @@ require "date"
 
 puts "EventManager initialized."
 
-hour_arr = []
+day_arr = []
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
@@ -11,16 +11,20 @@ contents.each do |row|
   name = row[:first_name]
   last_name = row[:last_name]
   regdate = row[:regdate]
-  hours = DateTime.strptime(regdate, "%m/%d/%Y %H:%M").hour
-  hour_arr.push(hours)
+  day = DateTime.strptime(regdate, "%m/%d/%Y %H:%M")
+  weekday = Date.parse(day.to_s).strftime("%A")
+  day_arr.push(weekday)
 end
 
-hour_count = hour_arr.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total}
-sorted = hour_count.sort_by { |k,v| v }.to_h
-puts "Most used hours for registration are:"
+#print day_arr
+
+day_count = day_arr.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total}
+sorted = day_count.sort_by { |k,v| v }.to_h
+puts "Most used week days for registration are:"
 n = sorted.values.last
 sorted.each do |k, v|
   if v == n
     puts k
   end
 end
+
